5e2a8109d93408fb2ed0cc3e8a5d698195eb2f09
KMSDEV <nguyendaikhang2000@gmail.com>
2026-09-15 15:50:30 +0700

    clean .git

---
+++ lab1.js
@@ -0,0 +1,14 @@
+console.log("Đây là LAB 1");
+// PHP: $tenbien
+// JS: let, const
+// var: ko dùng var
+let name = "kmsdev";
+name = "kmsdev123";
+const age = 19;
+const diachi = "Thái Bình";
+const status = "HD";
+
+console.log("Tên tôi là:", name);
+console.log("Tuổi: ", age);
+console.log("Địa chỉ: ", diachi);
+console.log("Trạng thái sinh viên: ", status);
