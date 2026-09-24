077a3360389b7c9cd1b510faf1c33f5b2aafcbc4
KMSDEV <nguyendaikhang2000@gmail.com>
2026-09-24 14:42:18 +0700

    Lab 3

--- lab3.js
+++ lab3.js
@@ -22,3 +22,20 @@ Tuổi: ${student.age}`;
 
 console.log(studentinfo);
 
+// Bài tập lab 3
+
+const name1 = "Nguyễn Đại Khang";
+const age1 = 20;
+const major1 = "Lập trình Web";
+const info1 = "Ten: " + name1 + " Tuoi: " +age1 + " Major: " + major1;
+console.log(info1);
+
+const student1 = {
+    idd: 1,
+    name1: "Nguyễn Đại Khang",
+    age1: 20,
+    major: "Lập trình Web",
+};
+
+const mystudent1 = `Mã sinh viên: ${student1.idd} Họ và tên: ${student1.name1} Tuổi: ${student1.age1} Ngành: ${student1.major1}`;
+console.log(mystudent1);
