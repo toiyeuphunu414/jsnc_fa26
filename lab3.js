b0e6e912b975f6543e8f2bcf9a59c0d0fbc95fc2
KMSDEV <nguyendaikhang2000@gmail.com>
2026-09-24 14:55:38 +0700

    Lab 3

--- lab3.js
+++ lab3.js
@@ -22,6 +22,52 @@ Tuổi: ${student.age}`;
 
 console.log(studentinfo);
 
+const arr = [
+  {
+    id: 1,
+    name: "Nguyễn Văn An",
+    age: 20,
+  },
+];
+
+const arrInfo = `${arr[0].name}`;
+console.log(arrInfo);
+
+const html = `
+  <div>
+    <h3>${student.name}</h3>
+    <p>Mã: ${student.id}</p>
+    <p>Tuổi: ${student.age}</p>
+  </div>
+`;
+// document.getElementById("app").innerHTML = html;
+
+function add(a, b) {
+  return a + b;
+}
+// arrow function
+const add2 = (a, b) => {
+  return a + b;
+};
+
+const add3 = (a, b) => a + b; // return a + b
+console.log(add2(3, 4));
+console.log(add2(6, 4));
+
+// map
+const students = [
+  { id: 1, name: "An" }, // item
+  { id: 2, name: "Bình" }, // item
+  { id: 3, name: "Cường" }, // item
+];
+
+const mapInfo = students.map((item) => {
+  console.log(item);
+  return `ID: ${item.id} - Ten: ${item.name}`;
+});
+
+console.log(mapInfo);
+
 // Bài tập lab 3
 
 const name1 = "Nguyễn Đại Khang";
